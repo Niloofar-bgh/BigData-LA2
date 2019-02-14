2f7565e318ae41fbf6b8181b74895866829c47be
Niloofar-bgh <baghdadi.niloofar@gmail.com>
2019-02-14 12:00:30 -0500

    first function

--- answers/answer.py
+++ answers/answer.py
@@ -75,8 +75,32 @@ def basic_als_recommender(filename, seed):
     - coldStartStrategy: 'drop'
     Test file: tests/test_basic_als.py
     '''
-    print ("this is a test")
-    return 0
+    sc = SparkContext.getOrCreate(SparkConf().setMaster("local[*]"))
+	spark = SQLContext(sc)
+	lines = spark.read.text(filename).rdd
+	parts = lines.map(lambda row: row.value.split("::"))
+	ratingsRDD = parts.map(lambda p: Row(userId=int(p[0]), movieId=int(p[1]),
+                                     rating=float(p[2]), timestamp=int(p[3])))
+
+	# timestamp should have been int not float to let this command run!
+	ratings = spark.createDataFrame(ratingsRDD)		
+	(training, test) = ratings.randomSplit([0.8, 0.2], seed)
+	# Build the recommendation model using ALS on the training data
+	# Note we set cold start strategy to 'drop' to ensure we don't get NaN evaluation metrics
+	als = ALS(rank = 70 , maxIter=5, regParam=0.01,userCol="userId", itemCol="movieId", ratingCol="rating",
+          coldStartStrategy="drop").setSeed(seed)
+
+	model = als.fit(training)
+
+	# Evaluate the model by computing the RMSE on the test data
+	predictions = model.transform(test)
+	evaluator = RegressionEvaluator(metricName="rmse", labelCol="rating",
+                                predictionCol="prediction")
+
+	rmse = evaluator.evaluate(predictions)
+
+	#Result
+	return rmse
 
 def global_average(filename, seed):
     '''
